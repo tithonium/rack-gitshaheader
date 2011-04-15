c6f661b5a13a4600151594819310cd9dfabfaa6a
Martin Tithonium <martian@midgard.org>
2011-04-14 19:47:24 -0700

    Append the sha to the html body, if there is one.

--- lib/rack/git-sha-header.rb
+++ lib/rack/git-sha-header.rb
@@ -18,6 +18,19 @@ module Rack
     def call(env)
       status, headers, body = @app.call(env)
       headers['X-GitSHA'] = @sha
+      begin
+        case body
+          when ActionDispatch::Response
+            if body.content_type == 'text/html'
+              body.body << "\n<!-- #{@sha} -->\n"
+            end
+          when String
+            if headers['Content-Type'].include?('text/html')
+              body << "\n<!-- #{@sha} -->\n"
+            end
+        end
+      rescue
+      end
       [status, headers, body]
     end
     
